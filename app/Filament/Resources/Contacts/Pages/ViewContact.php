cf2fbda0211ef92be2a9cdc7d541b3ca25aba77a
nanu <ranusate19@gmail.com>
2026-04-19 18:12:06 +0800

    update

--- app/Filament/Resources/Contacts/Pages/ViewContact.php
+++ app/Filament/Resources/Contacts/Pages/ViewContact.php
@@ -13,7 +13,7 @@ class ViewContact extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            // EditAction::make(),
         ];
     }
 }
