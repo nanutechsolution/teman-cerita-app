5ba3dee44afd88f5c3f970274687de1fcde9b6b3
nanu <ranusate19@gmail.com>
2026-04-19 17:56:34 +0800

    Create filament-shield.resource_permission_prefixes_labels.php

---
+++ lang/id/filament-shield.resource_permission_prefixes_labels.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Shield Permission Labels
+ *
+ * Translate the values below to localize permission labels in your application.
+ */
+
+return [
+    'create' => 'Buat',
+    'delete' => 'Hapus',
+    'delete_any' => 'Hapus semua',
+    'feature_breaking' => 'Set Breaking',
+    'feature_headline' => 'Set Headline',
+    'force_delete' => 'Hapus permanen',
+    'force_delete_any' => 'Hapus permanen semua',
+    'publish_post' => 'Publish Post',
+    'reorder' => 'Urutkan ulang',
+    'replicate' => 'Duplikat',
+    'restore' => 'Pulihkan',
+    'restore_any' => 'Pulihkan semua',
+    'update' => 'Ubah',
+    'view' => 'Lihat',
+    'view_any' => 'Lihat semua',
+    'view_episodes_chart' => 'Pertumbuhan Konten (30 Hari Terakhir)',
+    'view_latest_episodes_widget' => 'Berita Masuk Terakhir',
+    'view_stats_overview' => 'Stats Overview',
+];
