fe02d912bb15fc6112755ced6d0c9de4ffc27fda
nanu <ranusate19@gmail.com>
2026-04-19 18:06:24 +0800

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,6 +34,7 @@
     </script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 
 <body class="min-h-screen bg-neutral-50 dark:bg-[#0f0f0f] text-neutral-900 dark:text-[#f1f1f1] font-sans selection:bg-red-500/30 selection:text-red-900 dark:selection:text-white overflow-x-hidden antialiased transition-colors duration-300"
@@ -266,6 +267,7 @@
     </footer>
 
     @stack('scripts')
+    @livewireScripts
 
 </body>
 
