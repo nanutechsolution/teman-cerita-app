ce7a130bdd40336182afb29e1a493397a7118fff
nanu <ranusate19@gmail.com>
2026-04-22 02:15:46 +0800

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -37,9 +37,9 @@
     <meta property="og:image" content="{{ $finalOgImage }}">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
-    
+
     {{-- Twitter Card (Ini yang memaksa gambar menjadi besar/banner) --}}
-    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:card" content="summary">
     <meta name="twitter:title" content="{{ $title ?? $settings['site_name'] ?? config('app.name') }}">
     <meta name="twitter:description" content="{{ $meta_description ?? $settings['site_description'] ?? 'Kanal berbagi informasi dan cerita dari Nusa Tenggara Timur.' }}">
     <meta name="twitter:image" content="{{ $finalOgImage }}">
